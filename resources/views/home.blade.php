e3cd19cbc4e6c1b76a5fb0b553194ead79e76261
Akhmad <akhmadshaleh@gmail.com>
2023-11-28 15:04:47 +0700

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,11 +17,36 @@
 
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <div class="p-10 prose">
+                <div class="flex flex-col-reverse lg:items-start gap-8 lg:flex-row lg:justify-between p-14">
 
-                    <h1>Hello world.</h1>
+                    <div class="prose">
+                        <h2>Laravel 10 Starter</h2>
+                        <p>Laravel 10 Starter includes tailwindcss, livewire and several standard features like user login, register and user management.</p>
+                        <h3>Feature</h3>
+                        <ul>
+                        <li>User Login</li>
+                        <li>User Register
+                        <li>User manager with CRUD functionality, pagination, search and sort</li>
+                        <li>Example resource</li>
+                        </ul>
+                        <h3>Example resource include</h3>
+                        <ul>
+                        <li>Table, Text input, Textarea, Select, Checkbox</li>
+                        <li>Auto code generator</li>
+                        <li>Email with unique validation</li>
+                        <li>Upload an avatar</li>
+                        <li>Contact picker (beta)</li>
+                        </ul>
 
-                    <img src="{{ asset('assets/images/illust1.svg') }}" class="object-center">
+                        <div class="mt-10">
+                            <p>Github : <a href="https://github.com/akhmads/laravel10-starter" target="_blank" class="text-indigo-600 no-underline hover:underline">https://github.com/akhmads/laravel10-starter</a></p>
+                            <x-hyco.link href="https://saweria.co/akhmads" class="no-underline flex flex-row items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon w-6 h-6" viewBox="0 0 512 512"><path d="M368 80h64a16 16 0 0116 16v34a46 46 0 01-46 46h-34M96 80h272v192a80 80 0 01-80 80H176a80 80 0 01-80-80V80h0zM64 416h336" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
+                                Buy Me a Coffee
+                            </x-hyco.link>
+                        </div>
+                    </div>
+                    <img src="{{ asset('assets/images/illust1.svg') }}" class="inline">
 
                 </div>
 
